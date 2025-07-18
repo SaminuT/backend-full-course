41c1a6f7179f8f28af1d86a560b4da6143f2fe6d
SaminuT <saminuhansaja.w@gmail.com>
2025-07-18 17:59:58 +0530

    working

--- chapter4/src/routes/todoRoutes.js
+++ chapter4/src/routes/todoRoutes.js
@@ -6,19 +6,20 @@ const router = express.Router()
 // Get all todos for logged-in user
 router.get('/', async (req, res) => {
     const todos = await prisma.todo.findMany({
-        where:{
+        where: {
             userId: req.userId
         }
     })
+
     res.json(todos)
 })
 
 // Create a new todo
 router.post('/', async (req, res) => {
     const { task } = req.body
-    
+
     const todo = await prisma.todo.create({
-        data:{
+        data: {
             task,
             userId: req.userId
         }
@@ -31,7 +32,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     const { completed } = req.body
     const { id } = req.params
-    
+
     const updatedTodo = await prisma.todo.update({
         where: {
             id: parseInt(id),
@@ -41,7 +42,6 @@ router.put('/:id', async (req, res) => {
             completed: !!completed
         }
     })
-
     res.json(updatedTodo)
 })
 
@@ -49,14 +49,13 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const { id } = req.params
     const userId = req.userId
-    
     await prisma.todo.delete({
         where: {
             id: parseInt(id),
             userId
         }
     })
-    
+
     res.send({ message: "Todo deleted" })
 })
 
